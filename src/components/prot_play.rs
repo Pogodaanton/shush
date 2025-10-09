9c5c5bba31cc25fb2ab93466176046ea4d18a0cd
Pogodaanton <8974778+Pogodaanton@users.noreply.github.com>
2025-10-09 20:07:37 +0200

    feat: Playback progress bar; Playback reset prev button

--- src/components/prot_play.rs
+++ src/components/prot_play.rs
@@ -1,18 +1,27 @@
 use crate::core::session::SessionRequest;
 use crate::error::{Error, ErrorKind};
+use librespot::metadata::audio::AudioItem;
 use librespot::playback::player::PlayerEvent;
+use std::ops::{RangeInclusive};
+use std::time::Instant;
 use tokio::sync::mpsc::UnboundedSender;
 
 #[derive(Debug)]
 pub struct ProtPlay {
     play_state: PlayState,
-    disable_play_button: bool,
+    disable_buttons: bool,
     session_request_sender: Option<UnboundedSender<SessionRequest>>,
+    audio_item: Option<Box<AudioItem>>,
+    seek_range: RangeInclusive<f32>,
+    last_known_seek: f32,
+    last_playback_start: Option<Instant>,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     PlayThatOneSong,
+    Previous,
+    Next,
     Pause,
     Resume,
     Ignore,
@@ -37,28 +46,38 @@ impl ProtPlay {
         Self {
             play_state: PlayState::default(),
             session_request_sender,
-            disable_play_button: false,
+            disable_buttons: false,
+            last_known_seek: 0.0,
+            seek_range: RangeInclusive::new(0.0,0.0),
+            audio_item: None,
+            last_playback_start: None,
         }
     }
 
     pub fn view(&self) -> iced::Element<'_, Message> {
-        iced::widget::row![
-            iced::widget::button(match self.play_state {
-                PlayState::Stopped => "Play",
-                PlayState::Playing => "Pause",
-                PlayState::Paused => "Resume",
-            })
-            .on_press({
-                if self.disable_play_button {
-                    Message::Ignore
-                } else {
-                    match self.play_state {
-                        PlayState::Stopped => Message::PlayThatOneSong,
-                        PlayState::Playing => Message::Pause,
-                        PlayState::Paused => Message::Resume,
+        log::debug!("view update!");
+        iced::widget::column![
+            iced::widget::progress_bar(self.seek_range.clone(), self.get_current_seek()),
+            iced::widget::row![
+                iced::widget::button("prev").on_press(Message::Previous),
+                iced::widget::button(match self.play_state {
+                    PlayState::Stopped => "Play",
+                    PlayState::Playing => "Pause",
+                    PlayState::Paused => "Resume",
+                })
+                .on_press({
+                    if self.disable_buttons {
+                        Message::Ignore
+                    } else {
+                        match self.play_state {
+                            PlayState::Stopped => Message::PlayThatOneSong,
+                            PlayState::Playing => Message::Pause,
+                            PlayState::Paused => Message::Resume,
+                        }
                     }
-                }
-            })
+                }),
+                iced::widget::button("next").on_press(Message::Next),
+            ]
         ]
         .into()
     }
@@ -71,6 +90,8 @@ impl ProtPlay {
             Message::Pause => return self.pause().unwrap_or_else(Self::report_error),
             Message::Resume => return self.resume().unwrap_or_else(Self::report_error),
             Message::Ignore => {}
+            Message::Previous => return self.previous().unwrap_or_else(Self::report_error),
+            Message::Next => return self.next().unwrap_or_else(Self::report_error),
         }
 
         Action::None
@@ -78,21 +99,44 @@ impl ProtPlay {
 
     pub fn librespot_update(&mut self, player_event: PlayerEvent) {
         match player_event {
-            PlayerEvent::Playing { .. } => {
-                self.disable_play_button = false;
+            PlayerEvent::Playing { position_ms, .. } => {
+                self.disable_buttons = false;
                 self.play_state = PlayState::Playing;
+                self.last_known_seek = position_ms as f32;
+                self.last_playback_start.replace(Instant::now());
             }
-            PlayerEvent::Paused { .. } => {
-                self.disable_play_button = false;
+            PlayerEvent::Paused { position_ms, .. } => {
+                self.disable_buttons = false;
                 self.play_state = PlayState::Paused;
+                self.last_known_seek = position_ms as f32;
+                self.last_playback_start = None;
             }
             PlayerEvent::Stopped { .. } => {
-                self.disable_play_button = false;
+                self.disable_buttons = false;
                 self.play_state = PlayState::Stopped;
+                self.last_known_seek = 0.0;
+                self.last_playback_start = None;
+                self.seek_range = RangeInclusive::new(0.0, 0.0);
             }
             PlayerEvent::EndOfTrack { .. } => {
-                self.disable_play_button = false;
+                self.disable_buttons = false;
                 self.play_state = PlayState::Stopped;
+                self.last_known_seek = 0.0;
+                self.last_playback_start = None;
+                self.seek_range = RangeInclusive::new(0.0, 0.0);
+                self.audio_item = None;
+            }
+            PlayerEvent::Seeked { position_ms, .. } => {
+                self.disable_buttons = false;
+                self.last_known_seek = position_ms as f32;
+
+                if let PlayState::Playing = self.play_state {
+                    self.last_playback_start.replace(Instant::now());
+                }
+            }
+            PlayerEvent::TrackChanged { audio_item } => {
+                self.seek_range = RangeInclusive::new(0.0, audio_item.duration_ms as f32);
+                self.audio_item.replace(audio_item);
             }
             _ => {}
         }
@@ -106,7 +150,7 @@ impl ProtPlay {
     }
 
     fn play_that_one_song(&mut self) -> Result<Action, Error> {
-        self.disable_play_button = true;
+        self.disable_buttons = true;
         let sender = self.session_sender()?;
         sender.send(SessionRequest::PlayThatOneTrack)?;
 
@@ -116,7 +160,7 @@ impl ProtPlay {
     }
 
     fn pause(&mut self) -> Result<Action, Error> {
-        self.disable_play_button = true;
+        self.disable_buttons = true;
         let sender = self.session_sender()?;
         sender.send(SessionRequest::Pause)?;
 
@@ -126,13 +170,42 @@ impl ProtPlay {
     }
 
     fn resume(&mut self) -> Result<Action, Error> {
-        self.disable_play_button = true;
+        self.disable_buttons = true;
         let sender = self.session_sender()?;
         sender.send(SessionRequest::Resume)?;
 
         log::info!("Resuming current song.");
 
         Ok(Action::None)
+    }
+
+    fn previous(&mut self) -> Result<Action, Error> {
+        self.disable_buttons = true;
+        let sender = self.session_sender()?;
+        sender.send(SessionRequest::Previous)?;
+
+        log::info!("Requesting previous song or song beginning.");
+
+        Ok(Action::None)
+    }
+
+    fn next(&mut self) -> Result<Action, Error> {
+        self.disable_buttons = true;
+        let sender = self.session_sender()?;
+        sender.send(SessionRequest::Next)?;
+
+        log::info!("Requesting next song.");
+
+        Ok(Action::None)
+    }
+
+    fn get_current_seek(&self) -> f32 {
+        let progress_since_last_playback_start =
+            self.last_playback_start.map_or(0.0, |last_playback_start| {
+                last_playback_start.elapsed().as_millis() as f32
+            });
+
+        self.last_known_seek + progress_since_last_playback_start
     }
 
     fn report_error(err: Error) -> Action {
--- src/core/session.rs
+++ src/core/session.rs
@@ -7,10 +7,11 @@ use librespot::oauth::OAuthToken;
 use librespot::playback::audio_backend;
 use librespot::playback::config::{AudioFormat, PlayerConfig};
 use librespot::playback::mixer::NoOpVolume;
-use librespot::playback::player::{Player, PlayerEvent};
+use librespot::playback::player::{Player, PlayerEvent, PlayerEventChannel};
 use librespot::protocol::authentication::AuthenticationType;
 use std::sync::Arc;
-use tokio::sync::mpsc;
+use std::time::Instant;
+use tokio::sync::{mpsc, Mutex};
 use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
 use tokio::task::JoinHandle;
 use tokio_stream::wrappers::UnboundedReceiverStream;
@@ -20,12 +21,15 @@ pub enum SessionRequest {
     Pause,
     Resume,
     Stop,
+    Previous,
+    Next,
 }
 
 #[derive(Debug)]
 pub struct Session {
     config: Option<Config>,
     session_request_handler: JoinHandle<()>,
+    librespot_event_handler: JoinHandle<()>,
 }
 
 impl Session {
@@ -52,19 +56,24 @@ impl Session {
         let player_event_receiver = player.get_player_event_channel();
         let player_event_receiver_stream = UnboundedReceiverStream::from(player_event_receiver);
 
-        let (session_request_sender, rx) = mpsc::unbounded_channel::<SessionRequest>();
-        let session_request_handler = tokio::spawn(Self::handle_session_requests(rx, player));
+        let playback_data: Arc<Mutex<PlaybackData>> = Arc::new(Mutex::new(PlaybackData::default()));
+        let (session_request_s, session_request_r) = mpsc::unbounded_channel::<SessionRequest>();
+
+        let librespot_event_handler =
+            tokio::spawn(Self::handle_librespot_events(player.get_player_event_channel(), playback_data.clone()));
+        let session_request_handler =
+            tokio::spawn(Self::handle_session_requests(session_request_r, player, playback_data));
 
         Ok((
             Self {
                 config: Config {
                     credentials: Some(credentials),
-                }
-                .into(),
+                }.into(),
                 session_request_handler,
+                librespot_event_handler,
             },
             player_event_receiver_stream,
-            session_request_sender,
+            session_request_s,
         ))
     }
 
@@ -88,27 +97,137 @@ impl Session {
 
     async fn handle_session_requests(
         mut rx: UnboundedReceiver<SessionRequest>,
-        player: Arc<Player>,
+        librespot_player: Arc<Player>,
+        playback_data: Arc<Mutex<PlaybackData>>,
     ) {
+        let mut player_man = PlayerMan::new(librespot_player, playback_data.clone());
+
         while let Some(req) = rx.recv().await {
             match req {
-                SessionRequest::PlayThatOneTrack => {
-                    Self::async_play_that_one_song(&player).await.unwrap()
-                }
-                SessionRequest::Pause => player.pause(),
-                SessionRequest::Resume => player.play(),
-                SessionRequest::Stop => player.stop(),
-            }
+                SessionRequest::PlayThatOneTrack => player_man.play_that_one_track().await,
+                SessionRequest::Pause => player_man.pause().await,
+                SessionRequest::Resume => player_man.play().await,
+                SessionRequest::Stop => player_man.stop().await,
+                SessionRequest::Previous => player_man.previous().await,
+                SessionRequest::Next => {}
+            };
         }
     }
 
-    async fn async_play_that_one_song(player: &Player) -> Result<(), Error> {
-        let track = SpotifyId::from_uri("spotify:track:2dQNBDqHumZS3tfzjrfUHi")?;
+    /// Handler for events stemming from librespot.
+    /// Most of the data received here is used to re-align internal data shared with the
+    /// session_request handler so that playback is not fully approximated.
+    /// That said, if it gets little to no use, this will be stripped out.
+    async fn handle_librespot_events(
+        mut rx: PlayerEventChannel,
+        playback_data: Arc<Mutex<PlaybackData>>,
+    ) {
+        while let Some(event) = rx.recv().await {
+            match event {
+                PlayerEvent::Playing { play_request_id: _, position_ms, track_id: _ } => {
+                    let mut playback_data = playback_data.lock().await;
+                    playback_data.set_seek(position_ms as u64);
+                    playback_data.set_playing();
+                }
+                PlayerEvent::Paused { position_ms, .. } => {
+                    let mut playback_data = playback_data.lock().await;
+                    playback_data.set_paused();
+                    playback_data.set_seek(position_ms as u64);
+                }
+                PlayerEvent::Stopped { .. } => {
+                    let mut playback_data = playback_data.lock().await;
+                    playback_data.set_paused();
+                    playback_data.set_seek(0);
+                }
+                PlayerEvent::Seeked { position_ms, .. } => {
+                    playback_data.lock().await.set_seek(position_ms as u64);
+                }
+                PlayerEvent::EndOfTrack { .. } => {
+                    // not sure if it always means paused tbh, needs further testing
+                    let mut playback_data = playback_data.lock().await;
+                    playback_data.set_paused();
+                    playback_data.set_seek(0);
+                }
+                _ => {}
+            }
+        }
+    }
+}
 
-        player.load(track, true, 0);
+struct PlayerMan {
+    player: Arc<Player>,
+    playback_data: Arc<Mutex<PlaybackData>>,
+}
+
+impl PlayerMan {
+    pub fn new(player: Arc<Player>, playback_data: Arc<Mutex<PlaybackData>>) -> Self {
+        Self {
+            player,
+            playback_data,
+        }
+    }
+
+    pub async fn load_and_play(&self, track: SpotifyId) {
+        self.player.load(track, true, 0);
+    }
+
+    pub async fn play(&self) {
+        self.player.play();
+    }
+
+    async fn play_that_one_track(&self) {
+        let track = SpotifyId::from_uri("spotify:track:2dQNBDqHumZS3tfzjrfUHi").unwrap();
+
+        self.load_and_play(track).await;
 
         log::info!("Playing...");
+    }
 
-        Ok(())
+    pub async fn pause(&self) {
+        self.player.pause();
+    }
+
+    pub async fn stop(&self) {
+        self.player.stop();
+    }
+
+    pub async fn previous(&mut self) {
+        let playback_data = self.playback_data.lock().await;
+        if playback_data.current_seek_position() > 2000 {
+            self.player.seek(0);
+        }
+    }
+}
+
+#[derive(Debug, Default)]
+struct PlaybackData {
+    is_playing: bool,
+    last_known_seek: u64,
+    last_playback_start: Option<Instant>,
+}
+
+impl PlaybackData {
+    pub fn current_seek_position(&self) -> u64 {
+        let progress_since_last_playback_start = self
+            .last_playback_start
+            .map(|last_playback_start| last_playback_start.elapsed().as_millis() as u64)
+            .unwrap_or(0);
+
+        self.last_known_seek + progress_since_last_playback_start
+    }
+
+    pub fn set_seek(&mut self, seek_position: u64) {
+        self.last_known_seek = seek_position;
+        self.last_playback_start = self.is_playing.then(Instant::now);
+    }
+
+    pub fn set_paused(&mut self) {
+        self.is_playing = false;
+        self.last_playback_start = None;
+    }
+
+    pub fn set_playing(&mut self) {
+        self.is_playing = true;
+        self.last_playback_start = Some(Instant::now());
     }
 }
